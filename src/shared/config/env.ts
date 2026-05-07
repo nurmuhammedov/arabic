7da4fb28993da1310b26260a4fd4b322cda66260
Muhammad <104998775+nurmuhammedov@users.noreply.github.com>
2026-05-07 12:46:02 +0500

    Simplify getBaseUrl function logic

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,18 +1,9 @@
 const getBaseUrl = () => {
-  const envUrl = import.meta.env.VITE_BASE_URL
-  if (typeof window !== 'undefined') {
-    const hostname = window.location.hostname
-    if (hostname !== 'localhost' && hostname !== '127.0.0.1') {
-      return `http://${hostname}:8080`
-    }
-    if (hostname === 'localhost' || hostname === '127.0.0.1') {
-      return 'http://localhost:8080'
-    }
-  }
-  return envUrl || 'http://localhost:8080'
+    return import.meta.env.VITE_BASE_URL || 'http://localhost:8080'
 }
 
 export const BASE_URL: string = getBaseUrl()
+
 export const IS_DEV: boolean = import.meta.env.VITE_IS_DEV === 'true' || false
 export const CLIENT_ID: string = import.meta.env.VITE_CLIENT_ID || 'client_id'
 export const CLIENT_SECRET: string = import.meta.env.VITE_CLIENT_SECRET || 'client_secret'
